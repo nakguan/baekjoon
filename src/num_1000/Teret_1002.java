89461194c5ff2370a47a77b768f038d24fb33f27
DESKTOP-J28RBSJ\nakgu <nakguan@gmail.com>
2017-07-25 19:26:48 +0900

    해결

--- src/num_1000/Teret_1002.java
+++ src/num_1000/Teret_1002.java
@@ -1,5 +1,36 @@
 package num_1000;
 
-public class Teret_1002 {
+import java.util.Scanner;
 
+public class Teret_1002 {
+	static int fibonacci(int n, int[] check)
+	{
+		if(n==0){
+			check[0]++;
+			return 0;
+		}else if(n==1){
+			check[1]++;
+			return 1;
+		} else{
+			return fibonacci(n-1,check) + fibonacci(n-2,check);
+		}
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] s = new int[n];
+		
+		for(int i=0;i<n;i++)
+			s[i] = sc.nextInt();
+		
+		for(int i=0;i<n;i++)
+		{
+			int[] check = new int[2];
+			fibonacci(s[i],check);
+			System.out.println(check[0]+" "+check[1]);
+		}
+		
+		sc.close();
+	}
 }
